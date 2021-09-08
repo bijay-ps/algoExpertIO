b92cb8cda1e6f11be65dacfcc3fb8b9c34a013d9
Bijay Singh <bijay.prakash7@yahoo.in>
2021-09-08 17:39:46 +0530

    added more program

--- validateSequence.js
+++ validateSequence.js
@@ -11,7 +11,7 @@ const seq2 = [5, 1, 25, 22, 6, -1, 8, 10];
     5. If potential seq array is exhausted with or before the main array then return true else return false
 */
 
-function validatSequence(arr, seq) {
+function validatSequence(array, sequence) {
   let ptr = 0;
   array.forEach((e) => {
     if (e === sequence[ptr] && ptr < sequence.length) {
@@ -21,8 +21,18 @@ function validatSequence(arr, seq) {
   return ptr === sequence.length;
 }
 
+function validatSequenceAlgoExpertCode(array, sequence) {
+  let arrIdx = 0,
+    seqIdx = 0;
+  while (arrIdx < array.length && seqIdx < sequence.length) {
+    if (array[arrIdx] === sequence[seqIdx]) seqIdx++;
+    arrIdx++;
+  }
+  return seqIdx === sequence.length;
+}
+
 const res1 = validatSequence(arr, seq1);
-const res1 = validatSequence(arr, seq2);
+const res2 = validatSequence(arr, seq2);
 
 console.log("res1: ", res1);
 console.log("res2: ", res2);
